1cd0ae37f375460f88fecd8ef0e7af3aae740f8c
philmatt91 <felipedesmattos@gmail.com>
2020-08-07 19:50:12 -0300

    Ultima mudança

---
+++ CC .NET/BairesDev_Challenge/BairesDev_Challenge/Controllers/ClientController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BairesDev_Challenge.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult ClientList()
+        {
+            return View();
+        }
+    }
+}
