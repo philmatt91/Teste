9ea44c272f4b176a99c967a23ef4677882a36819
philmatt91 <felipedesmattos@gmail.com>
2020-08-07 18:59:10 -0300

    Gay em sentimento

---
+++ CC .NET/BairesDev_Challenge/BairesDev_Challenge/Models/Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BairesDev_Challenge.Models
+{
+    [Serializable]
+    public class Client : ClientId
+    {
+        //public long PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CurrentRole { get; set; }
+        public string Country { get; set; }
+        public string Industry { get; set; }
+        public int? NumberOfRecommendations { get; set; }
+        public int? NumberOfConnections { get; set; }
+
+        public Client(long personId,string firstName,string lastName
+            ,string currentRole,string country,string industry,int? numberOfRecommendations,int? numberOfConnections) : base(personId)
+        {
+            PersonId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+            CurrentRole = currentRole;
+            Country = country;
+            Industry = industry;
+            NumberOfRecommendations = numberOfRecommendations;
+            NumberOfConnections = numberOfConnections;
+        }
+    }
+
+    [Serializable]
+    public class ClientId
+    {
+        public long PersonId { get; set; }
+
+        public ClientId(long personId)
+        {
+            PersonId = personId;
+        }
+    }
+}
---
+++ CC .NET/BairesDev_Challenge/BairesDev_Challenge/Models/ConfigurationSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BairesDev_Challenge.Models
+{
+    public class ConfigurationSettings
+    {
+        public string JsonFilePath { get; set; }
+        public string PreferredCountry { get; set; }
+        public int StepNumberOfConnections { get; set; }
+        public List<string> LatamCountries { get; set; }
+    }
+}
